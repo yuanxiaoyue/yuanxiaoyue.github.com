6d2589cc56604265c39a42a7838d36682616ac89
yuanxiaoyue <1361006140@qq.com>
2017-03-29 20:45:53 +0800

    -a

---
+++ src/shi2.js
@@ -0,0 +1,23 @@
+<html>
+
+    <head>  
+        <script src="js/table.js"></script>
+        <link href="css/table.css" rel="stylesheet">
+    </head>
+    <body>
+        <table id="tbl">
+            <tr>
+                <td>11</td>
+                <td>12</td>
+            </tr>
+            <tr>
+                <td>21</td>
+                <td>22</td>
+            </tr>
+            <tr>
+                <td>31</td>
+                <td>32</td>
+            </tr>
+        </table>
+    </body>
+</html>
---
+++ src/shi22.js
@@ -0,0 +1,5 @@
+var tdList=document querySelecitorAll('td')
+for(i=0;i<tdList.length;i++){
+	item=tdList[i];
+	
+}
